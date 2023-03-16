ffefc7b8fbebcbfe59d72ea5b8cf291f6d9472ed
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2023-03-16 15:27:41 +0300

    Added functions for pause, resume and consolidate PVs.

--- archiver.py
+++ archiver.py
@@ -4,19 +4,23 @@
 import json
 import requests
 
+REQUEST_PV_LIST = "getAllPVs"
+REQUEST_CONSOLIDATE = "consolidateDataForPV"
+REQUEST_PAUSE = "pauseArchivingPV"
+REQUEST_RESUME = "resumeArchivingPV"
+
 class Archiver():
     """ Class definition """
-    def __init__(self, **args):
+    def __init__(self, server_ip, **args):
         self.mgmt_port = 17665
         self.retrieval_port = 17668
         self.server_ip = ""
         self.mgmt_url = ""
         self.retrieval_url = ""
+        self.server_ip = server_ip
 
         for key, value in args.items():
-            if key == "server_ip":
-                self.server_ip = value
-            elif key == "mgmt_port":
+            if key == "mgmt_port":
                 self.mgmt_port = int(value)
             elif key == "retrieval_port":
                 self.retrieval_port = int(value)
@@ -25,24 +29,45 @@ class Archiver():
 
     def get_pv_list(self):
         """ Retrive list of currently archived PVs. """
-        request = requests.get(url=self.retrieval_url + "", params={"limits": -1}, timeout=-1)
+        request = requests.get(url=self.mgmt_url + REQUEST_PV_LIST, params={"limit": -1})
         if request.status_code != 200:
             return None
 
         return json.loads(request.text)
-    
-    def consolidate_data(self, **args):
+
+    def pause_pv(self, pv_name):
+        """ Pause archiving for a single PV. """
+        request = requests.get(url=self.mgmt_url + REQUEST_PAUSE, params={"pv": pv_name})
+        if request.status_code != 200:
+            return False
+        return True
+
+    def resume_pv(self, pv_name):
+        """ Pause archiving for a single PV. """
+        request = requests.get(url=self.mgmt_url + REQUEST_RESUME, params={"pv": pv_name})
+        if request.status_code != 200:
+            return False
+        return True
+
+    def consolidate_data(self, pv_name, target_storage):
         """ Consolidate the PV to the target storage. """
-        pv_name = ""
-        target_storage = ""
-        for key, value in args.items():
-            if key == "pv":
-                pv_name = value
-            elif key == "target":
-                target_storage = value
-            else:
-                raise KeyError("consolidate_data: Invalid key passed")
-        
         if pv_name == "" or target_storage == "":
             raise ValueError("consolidate_data: PV name or target location is empty.")
 
+        if self.pause_pv(pv_name) is False:
+            print(f"PV {pv_name} was not paused successfully.")
+            return
+
+        request = requests.get(url=self.mgmt_url + REQUEST_CONSOLIDATE, params={"pv": pv_name, "storage": target_storage}, timeout=-1)
+        if request.status_code != 200:
+            print(f"Consolidation for PV {pv_name} failed.")
+
+        if self.resume_pv(pv_name) is False:
+            print(f"PV {pv_name} was not resumed successfully.")
+            return
+
+    def consolidate_all(self, target="LTS"):
+        """ Consolidate all PVs in the server to the target storage (LTS by default) """
+        pv_names = self.get_pv_list()
+        for pv_name in pv_names:
+            self.consolidate_data(pv_name=pv_name, target_storage=target)
